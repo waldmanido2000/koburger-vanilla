017a78fbc559eabaa56915699f56943bf904a6c9
waldmanido2000 <112062207+waldmanido2000@users.noreply.github.com>
2023-01-01 13:09:26 +0200

    finished

--- scripts.js
+++ scripts.js
@@ -43,21 +43,73 @@ function addElement(parentId, cost) {
   const element = document.createElement("div");
   element.setAttribute("id", parentId + numOfChildren);
   parent.appendChild(element);
-  parent.style.height = 'fit-content';
+  parent.style.height = "fit-content";
   totalCost = totalCost + cost;
-  reprice()
+  reprice();
 }
 // remove elements in dom //
 function removeElement(parentId, cost) {
   const parent = document.getElementById(parentId);
   const numOfChildren = Number(parent.childElementCount);
-  if(numOfChildren>0){
+  if (numOfChildren > 0) {
     parent.removeChild(parent.lastChild);
     totalCost = totalCost - cost;
-    reprice()
+    reprice();
   }
 }
 //price calculation //
 function reprice() {
   priceElement.innerHTML = totalCost;
+}
+// confirmation //
+function confirm() {
+  const vegan = Number(
+    document.getElementById("canvas-vegan").childElementCount
+  );
+  const meat = Number(document.getElementById("canvas-meat").childElementCount);
+  if (vegan + meat === 0) {
+    alert("you must add at least one patty");
+  } else {
+    const order = document.createElement("div");
+    order.setAttribute("id", "order-details");
+    const closeButton = document.createElement("span");
+    closeButton.setAttribute("id", "close-button");
+    closeButton.setAttribute("onclick", "reset()");
+    closeButton.innerHTML = "X";
+    const orderHeader = document.createElement("h3");
+    orderHeader.innerHTML = "your order details";
+    const orderDetails = document.createElement("p");
+    orderDetails.innerHTML = "you order cost is: " + totalCost;
+    order.append(closeButton, orderHeader, orderDetails);
+
+    const lettuce = Number(
+      document.getElementById("canvas-lettuce").childElementCount
+    );
+    koburgerUi.appendChild(order);
+  }
+}
+// reset //
+function reset() {
+  const canvas = document.getElementById("canvas");
+  totalCost = 0;
+  priceElement.innerHTML = totalCost;
+  canvas.innerHTML = "";
+  document.getElementById("order-details").remove();
+  canvas.insertAdjacentHTML(
+    "afterbegin",
+    `<div id="top-bun"></div>
+                    <div id="canvas-supplements">
+                        <div id="canvas-lettuce"></div>
+                        <div id="canvas-onion"></div>
+                        <div id="canvas-tomato"></div>
+                        <div id="canvas-cheese"></div>
+                        <div id="canvas-pickles"></div>
+                    </div>
+                
+                    <div id="canvas-patties">
+                        <div id="canvas-vegan"></div>
+                        <div id="canvas-meat"></div>
+                    </div>
+                    <div id="bottom-bun"></div>`
+  );
 }
